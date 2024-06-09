383e4ad91e14c8a47f84f4b21f2be732d1b2934d
nischhal-nub <nischalbh51@gmail.com>
2024-06-09 15:32:13 +0545

    add:cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,22 @@
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
 
 export default async function Page() {
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
+    
     return (
         <>
+        <div className='md:flex'>
             <RevenueChart revenue={revenue} />
             <LatestInvoices latestInvoices={latestInvoices}/>
+        </div>
+        <div>
+            <CardWrapper />
+        </div>
         </>
     )
 }
