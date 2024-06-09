48f2297b19bad292eceb6bd38d996157efee8cb3
nischhal-nub <nischalbh51@gmail.com>
2024-06-09 15:00:34 +0545

    connect: sql from vercel and created revenue and latest invoices components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,16 @@
-const Page=()=>{
-    return <p>Dashboard Page</p>
-}
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
 
-export default Page
+export default async function Page() {
+    const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices();
+    return (
+        <>
+            <RevenueChart revenue={revenue} />
+            <LatestInvoices latestInvoices={latestInvoices}/>
+        </>
+    )
+}
