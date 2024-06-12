7902e1e10f8e524b5e9bfdb82f2f7116dab35922
nischhal-nub <nischalbh51@gmail.com>
2024-06-12 16:51:09 +0545

    feat: authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,31 +3,53 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 export type State = {
-    errors?:{
+    errors?: {
         customerId?: string[];
         amount?: string[];
-        status?:string[];
+        status?: string[];
     }
-    message?:string | null;
+    message?: string | null;
 }
 
+// ...
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string({
-        invalid_type_error:"Please select a customer."
+        invalid_type_error: "Please select a customer."
     }),
-    amount: z.coerce.number().gt(0,"Please add the amount greater than $0."),
-    status: z.enum(['pending', 'paid'],{
-        invalid_type_error:"Please select a status."
+    amount: z.coerce.number().gt(0, "Please add the amount greater than $0."),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: "Please select a status."
     }),
     date: z.date()
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
 const UpdateInvoice = FormSchema.omit({ id: true, date: true })
-export async function createInvoice(prevState:State, formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
     try {
         const validatedFields = CreateInvoice.safeParse({
             customerId: formData.get('customerId'),
@@ -36,20 +58,20 @@ export async function createInvoice(prevState:State, formData: FormData) {
         })
         // Check if the form data is valid according to the defined schema.
         // If the form data is not valid, return an error message and the fields with errors.
-        if(!validatedFields.success){
+        if (!validatedFields.success) {
             // "flatten" is a method of the zod.ParseError object,
             // it returns an object with field errors and their corresponding messages.
             // The "fieldErrors" property is a dictionary with the field names as keys
             // and the error messages as values.
-            return{
-                errors:validatedFields.error.flatten().fieldErrors,
+            return {
+                errors: validatedFields.error.flatten().fieldErrors,
                 // The "Missing fields, failed to create invoice" message is just a generic error message.
                 // The actual error message should be displayed to the user based on the field errors returned by the "flatten" method.
-                message:'Missing fields, failed to create invoice'
+                message: 'Missing fields, failed to create invoice'
             }
         }
 
-        const {customerId,amount,status}= validatedFields.data
+        const { customerId, amount, status } = validatedFields.data
         const amountInCents = amount * 100;
         const date = new Date().toISOString().split('T')[0]
 
@@ -68,40 +90,48 @@ export async function createInvoice(prevState:State, formData: FormData) {
     }
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
     "use server"
-    try{
-    const { customerId, amount, status } = UpdateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
+    try {
+        const validatedFields = UpdateInvoice.safeParse({
+            customerId: formData.get('customerId'),
+            amount: formData.get('amount'),
+            status: formData.get('status'),
+        });
 
-    const amountInCents = amount * 100;
+        if (!validatedFields.success) {
+            return {
+                errors: validatedFields.error.flatten().fieldErrors,
+                message: "Missing fileds, failed to edit invoice."
+            }
+        }
 
-    await sql`
+        const { customerId, amount, status } = validatedFields.data
+        const amountInCents = amount * 100;
+
+        await sql`
       UPDATE invoices
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
 
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
-    }catch(err){
-        return{
-            message:"Database error!! Couldn't update the invoice."
+        revalidatePath('/dashboard/invoices');
+        redirect('/dashboard/invoices');
+    } catch (err) {
+        return {
+            message: "Database error!! Couldn't update the invoice."
         }
     }
 }
 
 export async function deleteInvoice(id: string) {
     // throw new Error('Database error! Cannot be deleted.')
-    try{
-    await sql`DELETE from invoices WHERE id = ${id}`
-    revalidatePath('/dashboard/invoices')
-    }catch(err){
-        return{
-            message:"Database error!! Couldn't delete the invoice."
-            }
+    try {
+        await sql`DELETE from invoices WHERE id = ${id}`
+        revalidatePath('/dashboard/invoices')
+    } catch (err) {
+        return {
+            message: "Database error!! Couldn't delete the invoice."
+        }
     }
 }
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -22,9 +22,9 @@ export default function EditInvoiceForm({
   console.log(invoice)
 
   const id = invoice.id
-  const initialState = {message:null,errors:{}}
+  const initialState = { message: null, errors: {} }
   const updateInvoicewithId = updateInvoice?.bind(null, id)
-
+  const [state, dispatch] = useFormState(updateInvoicewithId, initialState)
   // const [state, dispatch] = useFormState(updateInvoicewithId, initialState)
   // const[]
   // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -35,7 +35,7 @@ export default function EditInvoiceForm({
 
 
   return (
-    <form action={updateInvoicewithId}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -48,6 +48,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              aria-describedby='customer-error'
             >
               <option value="" disabled>
                 Select a customer
@@ -59,6 +60,15 @@ export default function EditInvoiceForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id='customer-error' aria-atomic='true' aria-live='polite'>
+            {
+              state.errors?.customerId && state.errors.customerId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))
+            }
           </div>
         </div>
 
@@ -77,9 +87,19 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby='amount-error'
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+          </div>
+          <div id='amount-error' aria-atomic='true' aria-live='polite'>
+            {
+              state.errors?.amount && state.errors.amount.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))
+            }
           </div>
         </div>
 
@@ -98,6 +118,7 @@ export default function EditInvoiceForm({
                   value="pending"
                   defaultChecked={invoice.status === 'pending'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='status-error'
                 />
                 <label
                   htmlFor="pending"
@@ -114,6 +135,7 @@ export default function EditInvoiceForm({
                   value="paid"
                   defaultChecked={invoice.status === 'paid'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='status-error'
                 />
                 <label
                   htmlFor="paid"
@@ -122,6 +144,15 @@ export default function EditInvoiceForm({
                   Paid <CheckIcon className="h-4 w-4" />
                 </label>
               </div>
+            </div>
+            <div id='status-error' aria-atomic='true' aria-live='polite'>
+              {
+                state.errors?.status && state.errors.status.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))
+              }
             </div>
           </div>
         </fieldset>
